ab46532c2f3fd77b5a31f69aed05a9bd54706bdf
Rexilone <117955767+Rexilone@users.noreply.github.com>
2026-01-12 18:31:28 +0900

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,40 +1,57 @@
-{ config, pkgs, ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, inputs, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+#      ./hardware.nix
+      inputs.home-manager.nixosModules.home-manager
     ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs; };
+    users.rexilone = {
+      imports = [ ./home.nix ];
+    };
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = false;
   boot.loader = {
     efi = {
-      efiSysMountPoint = "/boot";    
+      canTouchEfiVariables = true;
     };
     grub = {
       enable = true;
-      device = "nodev";
       efiSupport = true;
-      efiInstallAsRemovable = true;
-      extraEntriesBeforeNixOS = false;
+      device = "nodev";
       useOSProber = true;
-      extraEntries = ''
-        menuentry "Reboot" {
-          reboot
-        }
-        menuentry "PowerOff" {
-          halt
-        }
-      '';
     };
   };
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  programs.fish.enable = true;
-  users.defaultUserShell = pkgs.fish;
+
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -56,97 +73,78 @@
     LC_TIME = "ru_RU.UTF-8";
   };
 
+  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "ru";
     variant = "";
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.lik = {
-    isNormalUser = true;
+  services.getty.autologinUser = "rexilone";
+  users.users.rexilone = {
     shell = pkgs.fish;
-    description = "lik";
-    extraGroups = [ "networkmanager" "wheel" ];
+    isNormalUser = true;
+    description = "Rexilone";
+    extraGroups = [ "networkmanager" "wheel" "vboxusers" ];
     packages = with pkgs; [];
   };
-
-  # Enable automatic login for the user.
-  services.getty.autologinUser = "lik";
+  
+  environment.loginShellInit = ''
+    if [ "$(tty)" = "/dev/tty1" ]; then
+      exec hyprland
+    fi
+  '';
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  
-  environment.loginShellInit = ''
-  if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
-    exec Hyprland
-  fi
-  '';
-#  services.xserver = {
-#    enable = true;
-#    
-#    layout = "us,ru";
-#    xkbOptions = "shift_alt_toggle";
-#
-#    displayManager.sddm = {
-#      enable = true;
-#      wayland.enable = true;
-#    };
-#
-#  };
 
-
-  gtk.iconCache.enable = true;
-  programs.hyprland.enable = true;
-
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # system
-    nftables
-    #bluetooth
-    bluez
-    bluez-tools
-    blueman
-    # Hyprland
-    neovim
-    hyprland
-    hyprlock
-    hyprpaper
-    hyprpicker
+    inputs.zen-browser.packages.${stdenv.hostPlatform.system}.default
+    brightnessctl
+    home-manager
     hyprshot
-    kitty
-    waybar
-    rofi
-    nwg-look
-    git
-    curl
+    hyprpicker
+    hyprlock
     fastfetch
+    pavucontrol
+    nwg-look
+    waybar
+    neovim
+    viewnior
+    kitty
+    btop
+    swww
+    rofi
     nemo
-    # Applications
-    firefox
-    telegram-desktop
-    vscode
-    # decoration
-    papirus-icon-theme
-    graphite-gtk-theme
-    bibata-cursors
-    # shell
-    fish
-    #unstbr
-    onlyoffice-bin
+    p7zip
+    mpv
+    git
   ];
 
-  
-    fonts.packages = with pkgs; [
-      font-awesome
-      font-awesome_5
-      jetbrains-mono
-      dejavu_fonts
-    ];
+  fonts.fontDir.enable = true;
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    font-awesome
+    font-awesome_5
+    jetbrains-mono
+    liberation_ttf
+    dejavu_fonts
+    corefonts
+  ];
 
-  environment.sessionVariables = {
-    XCURSOR_THEME = "Bibata-Modern-Ice";
-    XCURSOR_SIZE = "24";
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server
   };
 
+  virtualisation.virtualbox.host = {
+    enable = true;
+    enableExtensionPack = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -155,6 +153,16 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+
+  programs.fish.enable = true;
+  
+  programs.dconf.profiles.user.databases = [{
+    settings."org/gnome/desktop/interface".gtk-theme = "Space";
+  }];
+
+  environment.sessionVariables = {
+    GTK_THEME = "Space";
+  };
 
   # List services that you want to enable:
 
@@ -173,6 +181,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 
 }
