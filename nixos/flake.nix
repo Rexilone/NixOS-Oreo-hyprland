5622098328766d26c4f59a256b5a00ec8703308e
Rexilone <117955767+Rexilone@users.noreply.github.com>
2026-01-12 18:29:18 +0900

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,13 +3,13 @@
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		zen-browser.url = "github:youwen5/zen-browser-flake";
-
+		zapret-discord-youtube.url = "github:kartavkun/zapret-discord-youtube";
 		home-manager = {
 			url = "github:nix-community/home-manager/release-25.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
-	outputs = { self, nixpkgs, zen-browser, ... }@inputs:
+	outputs = { self, nixpkgs, zen-browser, zapret-discord-youtube, ... }@inputs:
 		let
 			system = "x86_64-linux";
 		in{
@@ -18,6 +18,14 @@
 			specialArgs = { inherit inputs system; };
 			modules = [
 			./configuration.nix
+			
+			zapret-discord-youtube.nixosModules.default
+        		{
+        			services.zapret-discord-youtube = {
+        			enable = true;
+        			config = "general(ALT11)";  # Или любой конфиг из папки configs (general, general(ALT), general (SIMPLE FAKE) и т.д.)
+        			};
+        		}
 			];
 		};
 	};
