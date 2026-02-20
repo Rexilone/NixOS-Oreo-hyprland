dfddb7b27a48b84db0e1231b38fbf31cd70a5a20
Rexilone <117955767+Rexilone@users.noreply.github.com>
2026-02-20 10:37:53 +0900

    Create kdeconnect.nix

---
+++ nixos/modules/programs/kdeconnect.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+  programs.kdeconnect.enable = true;
+  networking.firewall = {
+    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+    allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
+  };
+}
+
