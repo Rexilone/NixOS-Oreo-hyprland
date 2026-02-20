c1753a95dd54a72e2cf424a986454b9818b090d2
Rexilone <117955767+Rexilone@users.noreply.github.com>
2026-02-20 10:13:49 +0900

    Update home.nix

--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -1,1 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+
+  imports =
+    [
+      ./modules/apps/default.nix
+      ./modules/desktop/cursor.nix
+      ./modules/desktop/gtk.nix
+    ];
+
+	home = {
+		enableNixpkgsReleaseCheck = false;
+		stateVersion = "25.11";
+	};
+
+}
 
