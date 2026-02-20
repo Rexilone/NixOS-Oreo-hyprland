ac7f0a40705ae1569335170d7b390eb746714ddd
Rexilone <117955767+Rexilone@users.noreply.github.com>
2026-02-20 10:03:14 +0900

    Create boot.nix

---
+++ nixos/modules/core/boot.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "breeze"; 
+    };
+
+    # Параметры ядра для "тихой" загрузки (скрывают текст логов)
+    kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
+    # Использование systemd в initrd для более раннего запуска Plymouth
+    initrd.systemd.enable = true;
+  };
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+}
