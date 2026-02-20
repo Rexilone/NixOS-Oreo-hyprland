746fb86765d0a0fe1e78fa5238ea7531901affd6
Rexilone <117955767+Rexilone@users.noreply.github.com>
2026-02-20 10:03:35 +0900

    Update boot.nix

--- nixos/modules/core/boot.nix
+++ nixos/modules/core/boot.nix
@@ -9,7 +9,6 @@
       theme = "breeze"; 
     };
 
-    # Параметры ядра для "тихой" загрузки (скрывают текст логов)
     kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
     # Использование systemd в initrd для более раннего запуска Plymouth
     initrd.systemd.enable = true;
